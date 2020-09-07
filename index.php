9c2d9680ca102d6de1954a21f2471b32ff61d609
williamenrique <william21enrique@gmail.com>
2020-09-07 17:55:22 -0400

    incluir partes

--- index.php
+++ index.php
@@ -25,57 +25,7 @@ include './modules/include/head.php';
 		</style>
 
 		<!-- Navbar -->
-		<nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
-			<div class="container-fluid">
-				<!-- Navbar brand -->
-				<a class="navbar-brand" target="_blank" href="https://mdbootstrap.com/docs/standard/">
-					<img src="./src/img/logoWI.png" height="40" alt="" loading="lazy" style="margin-top: -3px;" />
-				</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarExample01"
-					aria-controls="navbarExample01" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarExample01">
-					<ul class="navbar-nav mr-auto mb-2 mb-lg-0">
-						<li class="nav-item active">
-							<a class="nav-link" aria-current="page" href="#intro">Home</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="https://www.youtube.com/channel/UC5CF7mLQZhvx8O5GODZAhdA" rel="nofollow"
-								target="_blank">Learn Bootstrap 5</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="https://mdbootstrap.com/docs/standard/" target="_blank">Download MDB UI KIT</a>
-						</li>
-					</ul>
 
-					<ul class="navbar-nav list-inline">
-						<!-- Icons -->
-						<li class="">
-							<a class="nav-link" href="https://www.youtube.com/channel/UC5CF7mLQZhvx8O5GODZAhdA" rel="nofollow"
-								target="_blank">
-								<i class="fab fa-youtube"></i>
-							</a>
-						</li>
-						<li class="">
-							<a class="nav-link" href="https://www.facebook.com/mdbootstrap" rel="nofollow" target="_blank">
-								<i class="fab fa-facebook-f"></i>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="https://twitter.com/MDBootstrap" rel="nofollow" target="_blank">
-								<i class="fab fa-twitter"></i>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="https://github.com/mdbootstrap/mdb-ui-kit" rel="nofollow" target="_blank">
-								<i class="fab fa-github"></i>
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
 		<!-- Navbar -->
 
 		<!-- Background image -->
@@ -277,46 +227,7 @@ include './modules/include/head.php';
 	<!--Main layout-->
 
 	<!--Footer-->
-	<footer class="bg-light text-lg-left">
-		<div class="py-4 text-center">
-			<a role="button" class="btn btn-primary btn-lg m-2"
-				href="https://www.youtube.com/channel/UC5CF7mLQZhvx8O5GODZAhdA" rel="nofollow" target="_blank">
-				Learn Bootstrap 5
-			</a>
-			<a role="button" class="btn btn-primary btn-lg m-2" href="https://mdbootstrap.com/docs/standard/" target="_blank">
-				Download MDB UI KIT
-			</a>
-		</div>
 
-		<hr class="m-0" />
-
-		<div class="text-center py-4 align-items-center">
-			<p>Follow MDB on social media</p>
-			<a href="https://www.youtube.com/channel/UC5CF7mLQZhvx8O5GODZAhdA" class="btn btn-primary m-1" role="button"
-				rel="nofollow" target="_blank">
-				<i class="fab fa-youtube"></i>
-			</a>
-			<a href="https://www.facebook.com/mdbootstrap" class="btn btn-primary m-1" role="button" rel="nofollow"
-				target="_blank">
-				<i class="fab fa-facebook-f"></i>
-			</a>
-			<a href="https://twitter.com/MDBootstrap" class="btn btn-primary m-1" role="button" rel="nofollow"
-				target="_blank">
-				<i class="fab fa-twitter"></i>
-			</a>
-			<a href="https://github.com/mdbootstrap/mdb-ui-kit" class="btn btn-primary m-1" role="button" rel="nofollow"
-				target="_blank">
-				<i class="fab fa-github"></i>
-			</a>
-		</div>
-
-		<!-- Copyright -->
-		<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-			© 2020 Copyright:
-			<a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-		</div>
-		<!-- Copyright -->
-	</footer>
 	<!--Footer-->
 	<?php include './modules/include/footer.php'?>
 </body>
