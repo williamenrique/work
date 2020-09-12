13525752184cc3012c31c789f5980f3bace278ef
williamenrique <william21enrique@gmail.com>
2020-09-12 11:25:17 -0400

    prueba imagen

--- index.php
+++ index.php
@@ -8,7 +8,12 @@
 		<div class="container">
 			<h1 class="text-center p-4">Servicios</h1>
 			<div class="box-grid row">
-				<div class="col-12 col-md-4">columna</div>
+				<div class="col-12 col-md-4">
+
+					<div class="cont-img">
+						<img src="./src/assets/img/logoWI.png" alt="" class="img-fluid">
+					</div>
+				</div>
 				<div class="col-12 col-md-4">columna</div>
 				<div class="col-12 col-md-4">columna</div>
 			</div>
