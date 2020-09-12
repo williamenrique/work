be79e5e1792434b288b5a264034e661dac111877
williamenrique <william21enrique@gmail.com>
2020-09-11 21:31:42 -0400

    main

--- index.php
+++ index.php
@@ -3,5 +3,17 @@
 
 
 <div class="banner"></div>
+<main>
+	<div class="box-servicio">
+		<div class="container">
+			<h1 class="text-center p-4">Servicios</h1>
+			<div class="box-grid row">
+				<div class="col-12 col-md-4">columna</div>
+				<div class="col-12 col-md-4">columna</div>
+				<div class="col-12 col-md-4">columna</div>
+			</div>
+		</div>
+	</div>
+</main>
 
 <?php include './src/include/footer.php'?>
